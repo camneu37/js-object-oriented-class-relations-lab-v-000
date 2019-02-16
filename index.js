6607fb068ba5728edf705ec13e3c27cde4ed81c2
camneu37 <neuner.camille@gmail.com>
2019-02-15 21:45:16 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,80 @@
+let store = { drivers: [], passengers: [], trips: []}
+
+let driverId = 0
+class Driver {
+  constructor(name){
+    this.name = name
+    this.id = ++driverId
+
+    store.drivers.push(this)
+  }
+
+  trips(){
+    return store.trips.filter(
+      function(trip) {
+        return trip.driverId === this.id
+      }.bind(this)
+    )
+  }
+
+  passengers(){
+    let arrayOfPassengers = []
+    this.trips().forEach(function(trip){
+      arrayOfPassengers.push(trip.passenger())
+    })
+    return arrayOfPassengers
+  }
+}
+
+let passengerId = 0
+class Passenger {
+  constructor(name){
+    this.name = name
+    this.id = ++passengerId
+
+    store.passengers.push(this)
+  }
+
+  trips(){
+    return store.trips.filter(
+      function(trip) {
+        return trip.passengerId === this.id
+      }.bind(this)
+    )
+  }
+
+  drivers(){
+    let arrayOfDrivers = []
+    this.trips().forEach(function(trip){
+      arrayOfDrivers.push(trip.driver())
+    })
+    return arrayOfDrivers
+  }
+}
+
+let tripId = 0
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+
+    store.trips.push(this)
+  }
+
+  driver(){
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId
+      }.bind(this)
+    )
+  }
+
+  passenger(){
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId
+      }.bind(this)
+    )
+  }
+}
